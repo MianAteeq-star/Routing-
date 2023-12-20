cbe1c0cfda63b70a76e2f794d3f2eed8ba7d1ae8
Mian Ateeq-star <mr.unknown5264@gmail.com>
2023-12-20 15:47:45 +0500

    Router dom and api fetch

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+export default function Github() {
+
+    const data = useLoaderData()
+    console.log(data);
+//     const [data,setData]= useState({})
+// useEffect(()=>{
+//   fetch("https://api.github.com/users/MianAteeq-star")
+//   .then((res)=> res.json())
+//   .then((data) => setData(data))
+//   console.log(data);
+// },[])
+
+  return (
+    <>
+    <div className=' flex flex-col  items-center justify-center text-center p-4 bg-gray-600 text-white'>Github Public-Repository : {data.public_repos}
+    
+<div className="mt-4"><img src={data.avatar_url} alt="github profile" width={300} /></div>
+    </div>
+
+    
+    </>
+  )
+}
+
+
+export const  GithubInfo= async()=>{
+ const res = await fetch("https://api.github.com/users/MianAteeq-star")
+return res.json()
+}
